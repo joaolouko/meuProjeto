2bb9188eb11d24bd383fc314393890415434e3dc
RedesIFC <yagofranciscomodelsebastiao@gmail.com>
2023-08-08 15:15:01 -0300

    lol

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ console.log("ola, esse vai ser o 6 commit")
 var log = document.getElementById("x")
 
 log.innerHTML = "ola"
+
+let decimo = document.getElementById("dec")
+
+decimo.addEventListener("click", algumacoisa)
+
+
+function algumacoisa(){
+    console.log("decimo commit")
+}
